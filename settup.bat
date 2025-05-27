b41bbe12cdb55cc5b3b81fa52be6603b9fb610f9
Q-Vortex <sceletonboy223@gmail.com>
2025-05-27 18:10:31 +0300

    Fixing issues

--- settup.bat
+++ settup.bat
@@ -1,16 +1,13 @@
 @echo off
-if exist "..\mapGen\init.sh" (
-    echo Initializing project...
+echo Initializing project...
 
-    call npm install
-    if %errorlevel% equ 0 (
-        call cd backend
-        call npm run dev
-    ) else (
-        echo npm install failed, attempting to install nodejs and npm...
-        echo You must install Node.js and npm manually on Windows.
-        echo For instructions, visit my GitHub page: https://github.com/Q-Vortex/PerlinLab
-    )
+call npm install
+
+if %errorlevel% equ 0 (
+    call cd backend
+    call npm run dev
 ) else (
-    echo Please ensure you are in the correct directory.
+    echo npm install failed, attempting to install nodejs and npm...
+    echo You must install Node.js and npm manually on Windows.
+    echo For instructions, visit my GitHub page: https://github.com/Q-Vortex/PerlinLab
 )
--- settup.sh
+++ settup.sh
@@ -1,17 +1,12 @@
-if [ -f ../mapGen/init.sh ]; then
-  echo "Initializing project..."
+echo "Initializing project..."
 
-  npm install
+npm install
 
-  if [ $? -eq 0 ]; then
-    cd backend
-    npm run dev
-  else
-    echo "npm install failed. Attempting to install Node.js and npm..."
-    sudo apt install -y nodejs npm
-    npm run dev
-  fi
+if [ $? -eq 0 ]; then
+  cd backend
+  npm run dev
 else
-  echo "Please ensure you are in the correct directory."
+  echo "npm install failed. Attempting to install Node.js and npm..."
+  sudo apt install -y nodejs npm
+  npm run dev
 fi
-
